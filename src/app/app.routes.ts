4be8a99e3dcf559fa2054da12275d8f011167dd2
laska-adief <laskaadief.la@gmail.com>
2023-12-19 07:50:13 +0700

    [Atomic Angular Todolist] config routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TodolistComponent } from './pages/todolist/todolist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TodolistComponent,
+  },
+];
