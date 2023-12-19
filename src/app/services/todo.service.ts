8a96fc9439f0eded2f02a0e7d2b65c380b6625ca
laska-adief <laskaadief.la@gmail.com>
2023-12-19 07:50:51 +0700

    [Atomic Angular Todolist] service Todo

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Todo } from '../../types/Todo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  private todos: Todo[] = [{ id: '1', title: 'app', completed: false }];
+  private todoListSubject = new BehaviorSubject<Todo[]>(this.todos);
+  todos$ = this.todoListSubject.asObservable();
+
+  constructor() {}
+
+  getTodos() {
+    return this.todos$;
+  }
+
+  addTodo(todo: Todo) {
+    const tempTodos = this.todos;
+    tempTodos.push(todo);
+    this.todoListSubject.next(tempTodos);
+  }
+
+  handleCheckedTodo(id: string) {
+    const tempTodo = this.todos;
+    const findTodo: Todo | any = tempTodo.find((todo: Todo) => todo.id === id);
+    findTodo.completed = !findTodo.completed;
+    this.todoListSubject.next(tempTodo);
+  }
+}
